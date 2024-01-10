8f72c1083880b2e3aa8a87cc5f7c6e80161f9c3b
Tomer-WD <tomeredri23@gmail.com>
2024-01-10 18:56:29 +0200

    add service

--- src/app/components/property/property-list/property-list.component.ts
+++ src/app/components/property/property-list/property-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.scss']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit{
+  properties: any;
 
+  constructor(private housingService: HousingService){}
+  ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data =>  {
+        this.properties = data;
+        console.log(data);
+      } , error => {
+        console.log(error)
+      }
+    )
+  }
 }
